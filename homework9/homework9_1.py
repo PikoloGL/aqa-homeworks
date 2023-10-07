3b1f4ccd2682f2c6fb105fe20b431f9445d94fd7
PikoloGL <mykola.radahuz@gmail.com>
2023-10-07 17:01:17 +0300

    Implemented fixes from comment

--- homework9/homework9_1.py
+++ homework9/homework9_1.py
@@ -4,37 +4,69 @@ Create a class describing any company. For example, Toshiba or Apple
 
 
 class Company:
+    """
+    A class to describe a company.
+
+    Attributes:
+        name (str): The name of the company.
+        founded_year (int): The year the company was founded.
+        industry (str): The industry in which the company operates.
+        headquarters_location (str): The location of the company's headquarters.
+        CEO (str): The name of the company's CEO.
+        employees (int): The number of employees in the company.
+        revenue (float): The annual revenue of the company in billions of dollars.
+    """
+
     def __init__(self, name, founded_year, industry, headquarters_location, CEO, employees, revenue):
+        """
+        Initializes a new Company instance.
+        """
         self.name = name
         self.founded_year = founded_year
         self.industry = industry
         self.headquarters_location = headquarters_location
-        self.CEO = CEO
-        self.employees = employees
-        self.revenue = revenue
+        self.CEO = CEO  # Public attribute
+        self._employees = employees  # Protected attribute
+        self.__revenue = revenue  # Private attribute
 
     def __str__(self):
+        """
+        Function that returns information about our company
+        """
         return f"Company Name: {self.name}\n" \
                f"Founded Year: {self.founded_year}\n" \
                f"Industry: {self.industry}\n" \
                f"Headquarters Location: {self.headquarters_location}\n" \
                f"CEO: {self.CEO}\n" \
-               f"Number of Employees: {self.employees}\n" \
-               f"Annual Revenue: ${self.revenue} billion"
+               f"Number of Employees: {self._employees}\n" \
+               f"Annual Revenue: ${self.__revenue} billion"
+
+    @property
+    def revenue(self):  # getter
+        return self.__revenue
+
+    @revenue.setter
+    def revenue(self, new_revenue):  # setter
+        self.__revenue = new_revenue
 
 
-# Example usage:
-
-# Create an instance of the Company class for Apple Inc.
-apple = Company(
-    name="Apple Inc.",
-    founded_year=1976,
-    industry="Technology",
-    headquarters_location="Cupertino, California, USA",
-    CEO="Tim Cook",
-    employees=147000,
-    revenue=365.7  # In billions of dollars (2021)
-)
-
-# Display information about Apple Inc.
-print(apple)
+if __name__ == "__main__":
+    """
+    Create an instance of the Company class for Apple Inc.
+    """
+    apple = Company(
+        name="Apple Inc.",
+        founded_year=1976,
+        industry="Technology",
+        headquarters_location="Cupertino, California, USA",
+        CEO="Tim Cook",
+        employees=147000,
+        revenue=365.7  # In billions of dollars (2021)
+    )
+    """
+    Display information about Apple Inc.
+    """
+    print(apple)
+    print(f"Old revenue: ${str(apple.revenue)} billion")
+    apple.revenue = 501.3
+    print(f"New revenue: ${str(apple.revenue)} billion")
--- homework9/homework9_2.py
+++ homework9/homework9_2.py
@@ -4,31 +4,66 @@ Create a class with a description of the worker. Any worker or employees.
 
 
 class Worker:
+    """
+        A class to describe a worker.
+
+        Class attributes:
+            company (str): Name of the company that worker is working in
+
+        Attributes:
+            name (str): Worker name
+            employee_id (int): Worker id in our database
+            position (str): Worker position in company
+            department (str): Worker department
+            salary (int): Worker salary
+        """
+    company = "Apple"
+
     def __init__(self, name, employee_id, position, department, salary):
+        """
+        Initializes a new Worker instance.
+        """
         self.name = name
-        self.employee_id = employee_id
+        self._employee_id = employee_id  # Protected attribute
         self.position = position
         self.department = department
-        self.salary = salary
+        self.__salary = salary  # Private attribute
+        self.company = Worker.company
 
     def __str__(self):
+        """
+        Function that returns information about our worker
+        """
         return f"Employee Name: {self.name}\n" \
-               f"Employee ID: {self.employee_id}\n" \
+               f"Company Name: {self.company}\n" \
+               f"Employee ID: {self._employee_id}\n" \
                f"Position: {self.position}\n" \
                f"Department: {self.department}\n" \
-               f"Salary: ${self.salary}"
+               f"Salary: ${self.__salary}"
+
+    """
+    Static method that shows day offs across the company
+    """
+
+    @staticmethod
+    def day_off():
+        return "Day offs in company: 1.01, 25.12"
 
 
-# Example usage:
+if __name__ == "__main__":
+    """
+    Create an instance of the Worker class for an employee
+    """
+    employee1 = Worker(
+        name="John Doe",
+        employee_id="EMP123",
+        position="Software Engineer",
+        department="Engineering",
+        salary=80000  # Annual salary in dollars
+    )
 
-# Create an instance of the Worker class for an employee
-employee1 = Worker(
-    name="John Doe",
-    employee_id="EMP123",
-    position="Software Engineer",
-    department="Engineering",
-    salary=80000  # Annual salary in dollars
-)
-
-# Display information about the employee
-print(employee1)
+    """
+    Display information about the employee
+    """
+    print(employee1)
+    print(Worker.day_off())
